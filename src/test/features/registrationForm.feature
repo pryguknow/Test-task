8a6beaec85b3718b362dc3c297216a0dcd623d32
hlibpryhunov <h.pryhunov@mychip.io>
2024-06-06 16:47:51 +0200

    Created draft steps

--- src/test/features/registrationForm.feature
+++ src/test/features/registrationForm.feature
@@ -7,25 +7,26 @@ Feature: Filling out the student registration form
       And User enter first name as "<firstName>"
       And User enter last name as "<lastName>"
       And User enter email as "<email>"
-      And User selects gender
+      And User selects gender as "<gender>"
       And User enter mobile as "<mobile>"
-      And User selects date of birth as "<birthday>"
+      And User selects date of birth as "<dateOfBirthInput>"
       And User enter subjects
-      And User selects hobbies
+      And User selects hobbies as "<hobbies>"
       And the user attaches the picture
       And User enter current address as "<address>"
-      And User select state
-      And User select city
+      And User select state as "<state>"
+      And User select city as "<city>"
       And User click on the submit button
-      Then Form is filled out 
-      And User sees his data in the window with the questionnaire
+      Then User sees window with title "Thanks for submitting the form"
+      And The form displays correct information about the user
 
 
     Examples:
-      | firstName | lastName | email                    | mobile         | birthday    | addres        |
-      | Martin    | Test     | test123@mail.com         | 1234567890     | 11 Jun 1950 | Test str. 123b|
-      | lana      | test     | la,.natesnW2313@gmail.io | abc12345678901 | 24 Sep 2029 | test 22       |
-      | TeStOtEsT | De Test  | dddddd@dddd.co           | 0000000000@    | 30 Jun 2000 | @sss 44ab     |
+      | firstName | lastName    | email                      | gender | mobile         | dateOfBirthInput | hobbies | address           | state        | city    |
+      | Martin    | Test        | test123@ma.il_-.com        | Male   | 1234567890     | 11 Jun 1950      | Sport   | Test str. 123b    | NCR          | Delhi   |
+      | lana      | test        | la-.09natesnW2313@gmail.io | Female | abc12345678901 | 24 Sep 2029      | Reading | test 22           | UttarPradesh | Merrut  |
+      | TeStOtEsT | De Test     | d_ddddd@d18.comtt          | Other  | 0000000000@    | 30 Jun 2000      | Music   | @sss 44ab         | Haryana      | Panipat |
+      | tets22    | de test test| 111qate_-st@mm2442wd.cc    | Male   | 112233Q3456    | 01 Oct 1999      | Sport   | sdsaddsadasda_-2@ | Rajasthan    | Jaipur  |
 
 
 
--- src/test/steps/registrationFormSteps.ts
+++ src/test/steps/registrationFormSteps.ts
@@ -1,43 +1,107 @@
 import {Given, When, Then} from "@cucumber/cucumber"
 import { pageFixture } from "../../hooks/pageFixture";
+import { expect } from "@playwright/test";
 
 
 
 
 
 Given('User navigates to the application', async function () {
-    await pageFixture.page.goto("https://demoqa.com/automation-practice-form")
+    await pageFixture.page.goto("https://demoqa.com/automation-practice-form");
     await pageFixture.page.waitForLoadState();
+    await pageFixture.page.isVisible(".practice-form-wrapper");
 });
        
 Given('User enter first name as {string}', async function (firstName) {
-    
+    await pageFixture.page.locator("#firstName").fill(firstName);
 });
           
-Given('User enter last name as {string}', async function (string) {});
+Given('User enter last name as {string}', async function (lastName) {
+    await pageFixture.page.locator("#lastName").fill(lastName);
+});
              
-Given('User enter email as {string}', async function (string) {});
+Given('User enter email as {string}', async function (email) {
+    await pageFixture.page.locator("#userEmail").fill(email);
+});
        
-Given('User selects gender', async function () {});
+Given('User selects gender as {string}', async function (gender) {
+    const gendersSelcetors = {
+        Male: pageFixture.page.locator("9#gender-radio-1"),
+        Female: pageFixture.page.locator("#gender-radio-2"),
+        Other: pageFixture.page.locator("#gender-radio-3")
+    };
+    const genderSelector = gendersSelcetors[gender];
+    await genderSelector.click();
+});
        
-Given('User enter mobile as {string}', async function (string) {});
+Given('User enter mobile as {string}', async function (mobile) {
+    await pageFixture.page.locator("#userNumber").fill(mobile);
+});
              
-Given('User selects date of birth as {string}', async function (string) {});
+Given('User selects date of birth as {string}', async function (dateOfBirthInput) {
+    await pageFixture.page.locator("#dateOfBirthInput").fill(dateOfBirthInput);
+});
        
-Given('User enter subjects', async function () {});
+Given('User enter subjects', async function () {
+    const subjects = ["Math", "economics", "English"];
+    for (const nameOfSubject of subjects){
+        await pageFixture.page.locator("#subjectsContainer").fill(nameOfSubject);
+    }
+});
        
-Given('User selects hobbies', async function () {});
+Given('User selects hobbies as {string}', async function (hobbies) {
+    const hobbiesList = {
+        Sport: "#hobbies-checkbox-1",
+        Reading: "#hobbies-checkbox-2",
+        Music: "#hobbies-checkbox-3"
+    }
+    await pageFixture.page.locator(hobbiesList[hobbies]).click();
+});
        
-Given('the user attaches the picture', async function () {});
+Given('the user attaches the picture', async function () {
+    await pageFixture.page.locator("#uploadPicture").setInputFiles('../../../images/2F415E68-6AEB-484B-9029-05A7750D9254.JPG');
+});
              
-Given('User enter current address as {string}', async function (string) {});
+Given('User enter current address as {string}', async function (address) {
+    await pageFixture.page.locator("#currentAddress").fill(address);
+});
              
-Given('User select state', async function () {});
+Given('User select state as {string}', async function (state) {
+    const statesList = {
+        NCR: "#react-select-3-option-0",
+        UttarPradesh: "#react-select-3-option-1",
+        Haryana: "#react-select-3-option-2",
+        Rajasthan: "#react-select-3-option-3"
+    };
+    await pageFixture.page.locator("#state").click();
+    await expect(pageFixture.page.locator(".css-26l3qy-menu")).toBeVisible();
+    await pageFixture.page.locator(statesList[state]).click();
+    //expect(await pageFixture.page.locator(".css-11unzgr").isVisible()).toBeTruthy;
+});
            
-Given('User select city', async function () {});
+Given('User select city as {string}', async function (city) {
+    const cities = {
+        Delhi: "#react-select-4-option-0",
+        Merrut: "#react-select-4-option-2",
+        Panipat: "#react-select-4-option-1",
+        Jaipur: "#react-select-4-option-0"
+    };
+    await pageFixture.page.locator("#city").click();
+    await pageFixture.page.locator(cities[city]).click();
+
+    //const state = pageFixture.page.locator(".css-1uccc91-singleValue").innerText();
+
+});
               
-When('User click on the submit button', async function () {});
+When('User click on the submit button', async function () {
+    await pageFixture.page.locator("#submit").click();
+});
              
-Then('Form is filled out', async function () {});
+Then('User sees window with title "Thanks for submitting the form"', async function () {
+    await expect(pageFixture.page.locator(".modal-content")).toBeVisible();
+    await expect(pageFixture.page.locator("#example-modal-sizes-title-lg")).toHaveText("Thanks for submitting the form");
+});
        
-Then('User sees his data in the window with the questionnaire', async function () {});
+Then('The form displays correct information about the user', async function () {
+    console.log("success")
+});
