74046901da9edaf30a0089024980fc7e22643941
hlibpryhunov <h.pryhunov@mychip.io>
2024-06-09 14:43:28 +0200

    Change steps for test, add to hooks for screenshot

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,17 +1,36 @@
-import { Browser, Page, chromium } from "@playwright/test";
+import { Browser, BrowserContext, Page, chromium } from "@playwright/test";
 import { pageFixture } from "./pageFixture";
-import { After, AfterAll, Before, BeforeAll } from "@cucumber/cucumber";
+import { After, AfterAll, Before, BeforeAll, BeforeStep, AfterStep, Status } from "@cucumber/cucumber";
 
 let page: Page;
 let browser: Browser;
+let context: BrowserContext;
+
+BeforeAll( async function () {
+    browser = await chromium.launch({ headless:false });
+});
 
 Before(async function () {
-    browser = await chromium.launch({ headless:false });
+
+    context = await browser.newContext();
     page = await browser.newPage();
     pageFixture.page = page;
-})
+});
 
-// After(async function () {
-//     await page.close();
-//     await browser.close();
-// })
+After(async function ({ pickle, result}) {
+    //screenshot
+    if (result?.status == Status.FAILED){
+        const img = await pageFixture.page.screenshot({ path: `./test-result/screenshots/${pickle.name}Fail.png`, type: 'png'})
+        await this.attach(img, "image/png");
+    } else {
+        const img = await pageFixture.page.screenshot({ path: `./test-result/screenshots/${pickle.name}Success.png`, type: 'png'})
+        await this.attach(img, "image/png");
+    }
+
+    await pageFixture.page.close();
+    await context.close();
+});
+
+AfterAll(async function () {
+    await browser.close();
+});
--- src/test/steps/registrationFormSteps.ts
+++ src/test/steps/registrationFormSteps.ts
@@ -2,8 +2,7 @@ import { Given, When, Then, setDefaultTimeout } from "@cucumber/cucumber"
 import { pageFixture } from "../../hooks/pageFixture";
 import { expect } from "@playwright/test";
 import { statesListSelectors, hobbiesSelectors, genderSelectors } from "../../helper/locators/registrationFormPage"
-import { citiesList } from "../../helper/locators/registrationFormPage"
-//import path from "path";
+import { citiesList } from "../../helper/locators/registrationFormPage";
 import { createCitySelector } from "../../helper/scripts/givenSeletors";
 
 
@@ -16,31 +15,22 @@ Given('User navigates to the application', async function () {
     await pageFixture.page.isVisible(".practice-form-wrapper");
 });
    
-
 Given('User enter first name as {string}', async function (firstName) {
-
     await pageFixture.page.locator("#firstName").fill(firstName);
-
 });
     
-
 Given('User enter last name as {string}', async function (lastName) {
     await pageFixture.page.locator("#lastName").fill(lastName);
 });
  
-
 Given('User enter email as {string}', async function (email) {
     await pageFixture.page.locator("#userEmail").fill(email);
 });
       
-
 Given('User selects gender as {string}', async function (gender: keyof typeof genderSelectors) {
-   
     await pageFixture.page.click(`label[for=${genderSelectors[gender]}]`);
-
 });
        
-
 Given('User enter mobile as {string}', async function (mobile) {
     await pageFixture.page.locator("#userNumber").fill(mobile);
 });
@@ -63,41 +53,31 @@ Given('User selects hobbies as {string}', async function (hobbie: keyof typeof h
 
 });
        
-
 Given('the user attaches the picture', async function () {
 
     const fileChooserPromise = pageFixture.page.waitForEvent('filechooser');
     await pageFixture.page.locator('#uploadPicture').click();
     const fileChooser = await fileChooserPromise;
-    await fileChooser.setFiles('src/test/features/2F415E68-6AEB-484B-9029-05A7750D9254.JPG');
+    await fileChooser.setFiles('images/2F415E68-6AEB-484B-9029-05A7750D9254.JPG');
 
 });
              
 Given('User enter current address as {string}', async function (address) {
     await pageFixture.page.locator("#currentAddress").fill(address);
 });
-
-
-             
+          
 Given('User select {string} and {string}', async function (state: string, city: string) {
-   
     await pageFixture.page.locator("#state").click();
     await pageFixture.page.locator(statesListSelectors[state]).click();
     await pageFixture.page.click("#city");
-    //const citySelector: string = createCitySelector(citiesList, state, city);
     await pageFixture.page.locator(createCitySelector(citiesList, state, city)).click();
 });
-
-              
+          
 When('User click on the submit button', async function () {
     await pageFixture.page.click("#submit");
 });
              
-Then('User sees window with title "Thanks for submitting the form"', async function () {
-    await expect(pageFixture.page.locator(".modal-content")).toBeVisible();
-    await expect(pageFixture.page.locator("#example-modal-sizes-title-lg")).toHaveText("Thanks for submitting the form");
-});
-       
 Then('The form displays correct information about the user', async function () {
-    //make a screenshot
+    await pageFixture.page.waitForSelector(".modal-content");
+    await expect(pageFixture.page.locator("#example-modal-sizes-title-lg")).toHaveText("Thanks for submitting the form");
 });
